be4ed7d477d1e304203c651c747c42455bb438c0
Felix Fietkau <nbd@nbd.name>
2017-07-08 08:20:09 +0200

    debloat: [FIX] procfs
    Signed-off-by: Felix Fietkau <nbd@nbd.name>
    Signed-off-by: Eliminater74 <eliminater74@gmail.com>

--- mm/vmstat.c
+++ mm/vmstat.c
@@ -1867,12 +1867,12 @@ static int __init setup_vmstat(void)
 	cpu_notifier_register_done();
 #endif
 #ifdef CONFIG_PROC_FS
-	proc_create("buddyinfo", S_IRUGO, NULL, &fragmentation_file_operations);
-	proc_create("pagetypeinfo", S_IRUGO, NULL, &pagetypeinfo_file_ops);
+	if (!IS_ENABLED(CONFIG_PROC_STRIPPED)) {
+		proc_create("buddyinfo", S_IRUGO, NULL, &fragmentation_file_operations);
+		proc_create("pagetypeinfo", S_IRUGO, NULL, &pagetypeinfo_file_ops);
+		proc_create("zoneinfo", S_IRUGO, NULL, &proc_zoneinfo_file_operations);
+	}
 	proc_create("vmstat", S_IRUGO, NULL, &proc_vmstat_file_operations);
-	proc_create("zoneinfo", S_IRUGO, NULL, &proc_zoneinfo_file_operations);
-	proc_create("uid_lru_info", 0444, NULL,
-				&proc_uid_lru_info_file_operations);
 #endif
 	return 0;
 }
